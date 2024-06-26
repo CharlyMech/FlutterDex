09a08d152705f99b5e09991f406248d12f6fa703
carlos.sanchez <carlos.sanchez@omniaccess.com>
2024-06-26 17:20:59 +0200

    feat: rename generations screen file

---
+++ lib/screens/generations.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GenerationsScreen extends StatelessWidget {
+  const GenerationsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
