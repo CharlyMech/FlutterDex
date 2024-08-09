b9a969efac6e4fb97230f051130b3f5c79a4dbcb
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-08-09 21:08:40 +0200

    feat: create screens files

---
+++ lib/screens/items.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ItemsScreen extends StatelessWidget {
+  const ItemsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('ITEMS'),
+    );
+  }
+}
